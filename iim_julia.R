90e96f58bbc2102b4b2e14af9608815dcad806bc
Marc Baguelin <m.baguelin@imperial.ac.uk>
2023-10-14 08:44:45 +0100

    *Change value of c for the example *Improve IIM by having unsymetrical choice of the root of the polynomial *Remove axis and show c in plot

--- iim_julia.R
+++ iim_julia.R
@@ -6,7 +6,7 @@ x_minus_c <- x-x_c
 y_minus_c <- y-y_c
 
 rho <- (x_minus_c^2 + y_minus_c^2)^0.25
-phi <- atan2(y_minus_c, x_minus_c)/2 + rbinom(1,0.5)*3.141593
+phi <- atan2(y_minus_c, x_minus_c)/2 + rbinom(1,0.75)*3.141593
 
 update(x) <- rho*cos(phi)
 update(y) <- rho*sin(phi)
