ecdbb224f0543beb3f445f3e5279ab6acce8f140
Marc Baguelin <m.baguelin@imperial.ac.uk>
2025-05-08 09:57:20 +0100

    Code to generate simple julia set with odin2

--- moving_2_odin2.R
+++ moving_2_odin2.R
@@ -1,4 +1,20 @@
 library(odin2)
 library(dust2)
 
-sir <- odin2::odin("iim_julia.R")
+julia_mod <- odin2::odin("iim_julia.R")
+
+c <- c(-0.8,0.156)
+julia <- dust2::dust_system_create(generator = julia_mod,
+                                   pars=list(x_c=c[1], y_c=c[2]),
+                                   n_particles = 1000000)
+
+# for(i in 0:30){
+#   z <- julia$run(i)
+#   y_spread <- range(z[2,])
+#   plot(z[1,],z[2,],
+#        pch=".",
+#        col="blue")}
+
+julia_t <- dust_system_simulate(julia, times = 1:30)
+
+plot(julia_t[1,,25:30], julia_t[2,,25:30], pch=".")
