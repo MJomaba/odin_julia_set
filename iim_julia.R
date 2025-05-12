30f0f8620a1b72270d2ef21a5493155e251e6481
Marc Baguelin <m.baguelin@imperial.ac.uk>
2025-05-12 09:42:59 +0100

    Add p as parameter

--- iim_julia.R
+++ iim_julia.R
@@ -1,11 +1,12 @@
 x_c <- parameter()
 y_c <- parameter()
+p <- parameter(0.75)
 
 x_minus_c <- x-x_c
 y_minus_c <- y-y_c
 
 rho <- (x_minus_c^2 + y_minus_c^2)^0.25
-phi <- atan2(y_minus_c, x_minus_c)/2 + Binomial(1, 0.75)*3.141593
+phi <- atan2(y_minus_c, x_minus_c)/2 + Binomial(1, p)*3.141593
 
 update(x) <- rho*cos(phi)
 update(y) <- rho*sin(phi)
